a291f29207842cc2a750655dcf35961f0c75b67d
HarishKamaras <harishkamarasu@gmail.com>
2022-05-07 13:39:31 +0530

    createContacts

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
