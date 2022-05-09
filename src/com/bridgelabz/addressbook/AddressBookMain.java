9a226f1acd719f8023e154010cf98f1ffa9323cd
HarishKamaras <harishkamarasu@gmail.com>
2022-05-09 17:20:44 +0530

    DeleteContact

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -18,7 +18,6 @@ public class AddressBookMain {
             System.out.println("the"+contacts);
         }
         System.out.println("*********************");
-//
         //UC4 delete users
         addressBook.deleteContacts();
         for (Contacts contacts:addressBook.contactsArrayList) {
